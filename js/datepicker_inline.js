df18ab97a49015d2d8f5ea0470178d02a61072a4
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2025-10-11 17:59:59 +0300

    Correctly display calendar based on existing libraries

--- js/datepicker_inline.js
+++ js/datepicker_inline.js
@@ -1,50 +1,24 @@
 $(function() {
-      "use strict";
+  "use strict";
 
-      //Datepicker v1 embedded - https://www.daterangepicker.com/
-      /* var picker = $('#date_booking').daterangepicker({
-        parentEl: '#daterangepicker-embedded-container',
-        autoUpdateInput: false,
-        autoApply :true,
-        alwaysShowCalendars:true
-      });
-      // range update listener
-      picker.on('apply.daterangepicker', function(ev, picker) {
-        $(this).val(picker.startDate.format('MM-DD-YY') + ' to ' + picker.endDate.format('MM-DD-YY'));
-      });
-      // prevent hide after range selection
-      picker.data('daterangepicker').hide = function () {};
-      // show picker on load
-      picker.data('daterangepicker').show();
-      */
+  // Fetch booked dates from server first
+  fetch('php/get_ical.php')
+    .then(res => res.json())
+    .then(events => {
+      console.log('Booked dates from iCal:', events);
 
-      //Datepicker V2 embedded - - https://easepick.com/
-      $(function () {
-      /* Booked Dates */
-      const DateTime = easepick.DateTime;
-      const bookedDates = [
-        //Delete these 3 lines if not needed booked dates
-        ['2023-09-01', '2023-09-04'],
-        '2023-09-07',
-        ['2023-10-11', '2023-10-17'],
-      ].map(d => {
-        if (d instanceof Array) {
-          const start = new DateTime(d[0], 'YYYY-MM-DD');
-          const end = new DateTime(d[1], 'YYYY-MM-DD');
-
-          return [start, end];
-        }
-
-        return new DateTime(d, 'YYYY-MM-DD');
+      // Convert JSON to Easepick DateTime objects
+      const bookedDates = events.map(ev => {
+        const start = new easepick.DateTime(ev.start, 'YYYY-MM-DD');
+        const end   = new easepick.DateTime(ev.end, 'YYYY-MM-DD');
+        return [start, end];
       });
 
-      /* Configuration picker */
+      // Now create the picker with the booked dates
       const picker = new easepick.create({
         element: document.getElementById('date_booking'),
-        css: [
-          'css/daterangepicker_v2.css',
-        ],
-        lang: 'en-EN', // Language tags https://www.techonthenet.com/js/language_tags.php
+        css: ['css/daterangepicker_v2.css'],
+        lang: 'en-EN',
         format: "DD/MM/YYYY",
         calendars: 2,
         grid: 2,
@@ -52,13 +26,8 @@ $(function() {
         inline: true,
         plugins: ['LockPlugin', 'RangePlugin'],
         RangePlugin: {
-          tooltipNumber(num) {
-            return num - 1;
-          },
-          locale: {
-            one: 'night',
-            other: 'nights',
-          },
+          tooltipNumber(num) { return num - 1; },
+          locale: { one: 'night', other: 'nights' }
         },
         LockPlugin: {
           minDate: new Date(),
@@ -71,8 +40,9 @@ $(function() {
             }
             return date.inArray(bookedDates, '[)');
           }
-        },
+        }
       });
+    })
+    .catch(err => console.error('Error fetching booked dates:', err));
+});
 
-    }); // End Easypick config
-  });
--- php/get_ical.php
+++ php/get_ical.php
@@ -6,27 +6,42 @@ $ical_url = 'https://www.airbnb.com/calendar/ical/1312892551612331429.ics?s=124a
 
 // Fetch the iCal data
 $ical_data = file_get_contents($ical_url);
+if ($ical_data === false) {
+    echo json_encode([]);
+    exit;
+}
 
 $lines = explode("\n", $ical_data);
 $events = [];
 $event = [];
+
 foreach ($lines as $line) {
     $line = trim($line);
-    if ($line == 'BEGIN:VEVENT') {
+    if ($line === 'BEGIN:VEVENT') {
         $event = [];
-    } elseif ($line == 'END:VEVENT') {
-        if (isset($event['DTSTART']) && isset($event['DTEND'])) {
-            $events[] = [
-                'start' => $event['DTSTART'],
-                'end' => $event['DTEND']
-            ];
+    } elseif ($line === 'END:VEVENT') {
+        if (!empty($event['DTSTART']) && !empty($event['DTEND'])) {
+            // Convert to YYYY-MM-DD
+            $start = preg_replace('/T.*$/', '', $event['DTSTART']); // remove time if present
+            $end   = preg_replace('/T.*$/', '', $event['DTEND']);
+
+            $startDate = DateTime::createFromFormat('Ymd', $start);
+            $endDate   = DateTime::createFromFormat('Ymd', $end);
+
+            if ($startDate && $endDate) {
+                $events[] = [
+                    'start' => $startDate->format('Y-m-d'),
+                    'end'   => $endDate->format('Y-m-d')
+                ];
+            }
         }
     } else {
         if (strpos($line, ':') !== false) {
             list($key, $value) = explode(':', $line, 2);
-            $key = preg_replace('/;.*$/', '', $key); // Remove parameters
+            $key = preg_replace('/;.*$/', '', $key); // remove parameters like TZID
             $event[$key] = $value;
         }
     }
 }
+
 echo json_encode($events);
